478f7bc888533e76b201ed4db2f521b661a72d1f
sik <jsjsjskjs@gmail.com>
2022-03-04 18:01:44 +0900

    app.module edit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MoviesModule } from './movies/movies.module';
-import { AppController } from './app/app.controller';
-import { AppService } from './app/app.service';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [MoviesModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
