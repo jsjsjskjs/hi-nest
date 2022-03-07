dd792abd79f44ca95406f2acad4b729d51ea4734
sik <jsjsjskjs@gmail.com>
2022-03-07 11:31:09 +0900

    testing GET movies id

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,40 +2,57 @@ import { Test, TestingModule } from '@nestjs/testing'
 import { INestApplication } from '@nestjs/common'
 import * as request from 'supertest'
 import { AppModule } from './../src/app.module'
+import { ValidationPipe } from '@nestjs/common'
 
 describe('AppController (e2e)', () => {
   let app: INestApplication
 
-  beforeEach(async () => {
+  //beforeEach => beforeAll로 변경해줘야 각각의 test마다 새로 서버를 만들지 않고
+  //이전 테스트 기록이 그대로 이어진다
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule]
     }).compile()
 
     app = moduleFixture.createNestApplication()
+    //테스트 환경에서도 pipe를 동일하게 적용시켜줘야 한다!
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true
+      })
+    )
     await app.init()
   })
 
   describe('/movies', () => {
     it('GET', () => {
-      return request(app.getHttpServer())
-      .get('/movies')
-      .expect(200)
-      .expect([])
+      return request(app.getHttpServer()).get('/movies').expect(200).expect([])
     })
     it('POST', () => {
       return request(app.getHttpServer())
-      .post('/movies')
-      .send({
-        title: 'test',
-        year: 2000,
-        genres: ['test']
-      })
-      .expect(201)
+        .post('/movies')
+        .send({
+          title: 'test',
+          year: 2000,
+          genres: ['test']
+        })
+        .expect(201)
     })
     it('DELETE', () => {
-      return request(app.getHttpServer())
-      .delete('/movies')
-      .expect(404)
-    }) 
+      return request(app.getHttpServer()).delete('/movies').expect(404)
+    })
+  })
+
+  describe('/movies/:id', () => {
+    it('GET 200', () => {
+      return request(app.getHttpServer()).get('/movies/1').expect(200)
+    })
+    it('GET 404', () => {
+      return request(app.getHttpServer()).get('/movies/100').expect(404)
+    })
+    it.todo('DELETE')
+    it.todo('PATCH')
   })
 })
